18aeabe74dffc93b1d63853a5f62fde2f5aa9c43
Matvey Likhanov <lihanov.mat@gmail.com>
2025-10-10 17:17:27 +0300

    Implement DI container

--- cmd/grpc_server/main.go
+++ cmd/grpc_server/main.go
@@ -1,67 +1,21 @@
-// Package main implements the gRPC server
 package main
 
 import (
 	"context"
-	"flag"
 	"log"
-	"net"
 
-	"github.com/jackc/pgx/v5/pgxpool"
-	userApi "github.com/sborsh1kmusora/auth/internal/api/user"
-	"github.com/sborsh1kmusora/auth/internal/config"
-	userRepository "github.com/sborsh1kmusora/auth/internal/repository/user"
-	userService "github.com/sborsh1kmusora/auth/internal/service/user"
-	desc "github.com/sborsh1kmusora/auth/pkg/auth_v1"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
+	"github.com/sborsh1kmusora/auth/internal/app"
 )
 
-var configPath string
-
-func init() {
-	flag.StringVar(&configPath, "config-path", ".env", "path to config file")
-}
-
 func main() {
-	flag.Parse()
-
 	ctx := context.Background()
 
-	config.Load(configPath)
-
-	grpcConfig, err := config.NewGRPCConfig()
+	a, err := app.NewApp(ctx)
 	if err != nil {
-		log.Fatalf("Unable to load config: %v\n", err)
+		log.Fatalf("failed to initialize app: %v", err)
 	}
 
-	pgConfig, err := config.NewPGConfig()
-	if err != nil {
-		log.Fatalf("Unable to load config: %v\n", err)
-	}
-
-	lis, err := net.Listen("tcp", grpcConfig.Address()) // #nosec G102
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-
-	pool, err := pgxpool.New(ctx, pgConfig.DSN())
-	if err != nil {
-		log.Fatalf("Unable to parse config: %v\n", err)
-	}
-	defer pool.Close()
-
-	userRepo := userRepository.NewRepository(pool)
-	userSrv := userService.NewService(userRepo)
-
-	s := grpc.NewServer()
-	reflection.Register(s)
-
-	desc.RegisterAuthV1Server(s, userApi.NewImplementation(userSrv))
-
-	log.Printf("server listening at %v", lis.Addr())
-
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+	if err := a.Run(); err != nil {
+		log.Fatalf("failed to run: %v", err)
 	}
 }
---
+++ internal/app/app.go
@@ -0,0 +1,87 @@
+package app
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"github.com/sborsh1kmusora/auth/internal/closer"
+	"github.com/sborsh1kmusora/auth/internal/config"
+	desc "github.com/sborsh1kmusora/auth/pkg/auth_v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type App struct {
+	serviceProvider *serviceProvider
+	grpcServer      *grpc.Server
+}
+
+func NewApp(ctx context.Context) (*App, error) {
+	a := &App{}
+
+	if err := a.initDeps(ctx); err != nil {
+		return nil, err
+	}
+
+	return a, nil
+}
+
+func (a *App) Run() error {
+	defer func() {
+		closer.CloseAll()
+		closer.Wait()
+	}()
+
+	return a.runGRPCServer()
+}
+
+func (a *App) initDeps(ctx context.Context) error {
+	inits := []func(context.Context) error{
+		a.initConfig,
+		a.initServiceProvider,
+		a.initGRPCServer,
+	}
+
+	for _, f := range inits {
+		if err := f(ctx); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (a *App) initConfig(ctx context.Context) error {
+	config.Load(".env")
+	return nil
+}
+
+func (a *App) initServiceProvider(ctx context.Context) error {
+	a.serviceProvider = newServiceProvider()
+	return nil
+}
+
+func (a *App) initGRPCServer(ctx context.Context) error {
+	a.grpcServer = grpc.NewServer()
+	reflection.Register(a.grpcServer)
+
+	desc.RegisterAuthV1Server(a.grpcServer, a.serviceProvider.UserImpl(ctx))
+
+	return nil
+}
+
+func (a *App) runGRPCServer() error {
+	log.Printf("GRPC server is running on %s", a.serviceProvider.GRPCConfig().Address())
+
+	lis, err := net.Listen("tcp", a.serviceProvider.GRPCConfig().Address())
+	if err != nil {
+		return err
+	}
+
+	if err := a.grpcServer.Serve(lis); err != nil {
+		return err
+	}
+
+	return nil
+}
